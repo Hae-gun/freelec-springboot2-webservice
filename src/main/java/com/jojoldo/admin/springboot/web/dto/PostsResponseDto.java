101aac09c59b6fd02dccfb32197d4f0603b3c611
Hae-Gun <newchk610@gmail.com>
2020-08-31 12:08:12 +0900

    today

--- src/main/java/com/jojoldo/admin/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/jojoldo/admin/springboot/web/dto/PostsResponseDto.java
@@ -2,7 +2,9 @@ package com.jojoldo.admin.springboot.web.dto;
 
 import com.jojoldo.admin.springboot.domain.post.Posts;
 import lombok.Getter;
+import lombok.ToString;
 
+@ToString
 @Getter
 public class PostsResponseDto {
     private Long id;
@@ -11,6 +13,7 @@ public class PostsResponseDto {
     private String author;
 
     public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getAuthor();
